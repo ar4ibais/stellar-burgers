c08a29b6a93ab1d71f21ab33b2b8e8801158689e
Arthur <ak.hutov@yandex.ru>
2025-06-10 12:39:10 +0300

    поменял тесты, сделал названия на русском

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,125 +1,55 @@
-import { TIngredient } from '../../src/utils/types';
+describe('Бургер Конструктор', () => {
+  const INGREDIENTS_API = 'api/ingredients';
+  const INGREDIENTS_FIXTURE = 'ingredients.json';
 
-describe('Burger Constructor Integration Tests', () => {
-  let ingredientsData: any;
-
-  before(() => {
-    cy.fixture('ingredients.json').then((data) => {
-      ingredientsData = data;
-    });
-  });
-
-  const setupTestEnvironment = () => {
-    cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' }).as(
-      'fetchIngredients'
+  beforeEach(() => {
+    cy.intercept('GET', INGREDIENTS_API, { fixture: INGREDIENTS_FIXTURE }).as(
+      'getIngredients'
     );
     cy.visit('/');
-    cy.wait('@fetchIngredients');
-  };
+    cy.wait('@getIngredients');
+  });
 
-  const openIngredientModal = (index = 0) => {
+  const openModal = () => {
     cy.get("[data-cy='ingredients-items']")
       .first()
       .find("[data-cy='ingredient-container']")
-      .eq(index)
+      .first()
       .click();
   };
 
-  const verifyIngredientDetailsInModal = (ingredient: TIngredient) => {
-    cy.get("[data-cy='ingredient-details-name']").should(
-      'contain',
-      ingredient.name
-    );
-    cy.get("[data-cy='ingredient-details-image']").should(
-      'have.attr',
-      'src',
-      ingredient.image_large
-    );
-    cy.get("[data-cy='ingredient-details-calories']").should(
-      'contain',
-      ingredient.calories
-    );
-    cy.get("[data-cy='ingredient-details-proteins']").should(
-      'contain',
-      ingredient.proteins
-    );
-    cy.get("[data-cy='ingredient-details-fat']").should(
-      'contain',
-      ingredient.fat
-    );
-    cy.get("[data-cy='ingredient-details-carbs']").should(
-      'contain',
-      ingredient.carbohydrates
-    );
-  };
-
-  beforeEach(() => {
-    setupTestEnvironment();
-  });
-
-  describe('Ingredient Management', () => {
-    it('should correctly display empty state before adding ingredients', () => {
+  describe('Добавление ингредиентов в конструктор', () => {
+    it('должен отображать пустые состояния для булок и начинок', () => {
       cy.get("[data-cy='noBuns']").should('have.length', 3);
     });
 
-    it('should add correct ingredient to constructor when dragged', () => {
-      const testIngredient = ingredientsData.data[0];
+    it('должен добавлять ингредиенты в конструктор', () => {
+      cy.clickIngredientInIndexedContainer(0);
+      cy.clickIngredientInIndexedContainer(1);
 
-      // Add ingredient to constructor
-      cy.get("[data-cy='ingredients-items']")
-        .contains(testIngredient.name)
-        .trigger('dragstart');
-
-      cy.get("[data-cy='burger-constructor']")
-        .trigger('drop')
-        .trigger('dragend');
-
-      // Verify correct ingredient was added
-      cy.get("[data-cy='burger-constructor-element']")
-        .should('contain', testIngredient.name)
-        .and('be.visible');
+      cy.get("[data-cy='burger-constructor-element']").should('be.visible');
+      cy.get("[data-cy='burger-constructor-element-fullwidth']").should(
+        'be.visible'
+      );
     });
   });
 
-  describe('Ingredient Modal Functionality', () => {
-    it('should display correct ingredient details in modal', () => {
-      const testIngredient = ingredientsData.data[1];
-      openIngredientModal(1);
-
+  describe('Модальное окно ингредиента', () => {
+    beforeEach(() => {
+      openModal();
       cy.checkModalViibility(true);
-      verifyIngredientDetailsInModal(testIngredient);
     });
 
-    it('should close modal when close button is clicked', () => {
-      openIngredientModal();
-      cy.checkModalViibility(true);
+    it('должно закрываться при клике на кнопку закрытия', () => {
+      cy.get("[data-cy='modal']").find('button').click();
 
-      cy.get("[data-cy='modal-close-button']").click();
       cy.checkModalViibility(false);
     });
 
-    it('should close modal when overlay is clicked', () => {
-      openIngredientModal();
-      cy.checkModalViibility(true);
-
+    it('должно закрываться при клике на оверлей', () => {
       cy.get("[data-cy='modal-overlay']").click({ force: true });
+
       cy.checkModalViibility(false);
-    });
-  });
-
-  describe('API Request Optimization', () => {
-    it('should fetch ingredients only once on initial app load', () => {
-      cy.intercept('GET', 'api/ingredients').as('ingredientsRequest');
-      cy.visit('/');
-
-      cy.wait('@ingredientsRequest').then((interception) => {
-        expect(interception.request.url).to.include('api/ingredients');
-      });
-
-      // Verify no additional requests are made
-      cy.get("[data-cy='header-constructor-link']").click();
-      cy.get("[data-cy='header-feed-link']").click();
-      cy.get('@ingredientsRequest.all').should('have.length', 1);
     });
   });
 });
